278db771447ed9311dc4fca9ef7dc2c8b94d16fb
Ravi <rkm4198@gmail.com>
2022-06-04 18:16:55 +0530

    Added comment to BubleSort

--- BubleSort.py
+++ BubleSort.py
@@ -3,10 +3,11 @@ def bubleSort(nums):
     for i in range(len(nums) - 1, 0, -1):
         for j in range(i):
             if nums[j] > nums[j + 1]:
+		# Swapping value if first number is greater then second number
                 temp = nums[j]
                 nums[j] = nums[j + 1]
                 nums[j + 1] = temp
-                print(numbers)
+               # print(numbers)
 
 
 numbers = [5, 3, 8, 6, 7, 2]
